5451edf316b644f5f3b19cc2320c0217f86f6aeb
s <s>
2022-07-04 18:39:47 +0500

    get userAgent

---
+++ main.py
@@ -0,0 +1,17 @@
+from fake_useragent import UserAgent
+import requests
+
+
+ua = UserAgent()
+print(ua.random)
+
+
+def collect_data():
+	pass
+
+def main():
+	collect_data()
+
+
+if __name__ == '__main__':
+	main()
