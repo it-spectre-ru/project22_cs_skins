dfcb3f12568b5cf14e1a0cc57c0c0bce135c141b
s <s>
2022-07-04 19:02:21 +0500

    get json

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from fake_useragent import UserAgent
 import requests
-
+import json
 
 ua = UserAgent()
-print(ua.random)
+# print(ua.random)
+url = 'https://inventories.cs.money/5.0/load_bots_inventory/730?buyBonus=35&isStore=true&limit=60&maxPrice=10000&minPrice=1&offset=420&sort=botFirst&type=2&withStack=true'
+headers = {'user-agent': f'{ua.random}'}
+
 
 
 def collect_data():
-	pass
+	response = requests.get(url=url, headers=headers)
+
+	with open('result.json', 'w') as file:
+		json.dump(response.json(), file, indent=4, ensure_ascii=False)
 
 def main():
 	collect_data()
