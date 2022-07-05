89c3eb2883df787fa51443f4a4eb54f61eb34e38
s <s>
2022-07-05 10:02:04 +0500

    start bot, not working

---
+++ csmoney_bot.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, executor, types
+import os
+
+bot = Bot(token=os.getenv('TOKEN'), parse_mode=types.ParseMode.HTML)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+  start_buttons = ['🔪 Ножи', '🥊 Перчатки', '🔫 Снайперские винтовки']
+  keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  keyboard.add(*start_buttons)
+
+  await message.answer('Выберите категорию', reply_markup=keyboard)
+
+def main():
+  executor.start_polling(dp)
+
+if __name__ == 'main':
+  main()
+
+  
